60df279418418791eaef61ce6fc2cea216aaf1a1
Simon Slamka <51794014+simonSlamka@users.noreply.github.com>
2021-02-18 13:04:16 +0100

    Quest current build;

---
+++ Hellohl - Quest/Assets/Jetdogs-Prefabs-Udon/CHANGELOG.md
@@ -0,0 +1,82 @@
+V1.3.1
+*fixed graph and U# keypads.
+*moved ui panel example to prefabs folder.
+*changed ui to navigation none.
+*U# local transform start event fixed.
+*added interact event to synced toggle U#
+*Fixed twoway teleport U#.
+
+V1.3.0
+*UdonSharp expansion update. (I have moved around some files and added folders. If you already have my prefabs in your project i suggest deleting them and importing this new package for updated locations.)
+*seperated prefabs into 3 folders(prefabs, graph prefabs, udonsharp prefabs).
+*look in "Jetdogs-Prefabs-Udon > Udonsharp prefabs" folder for all the new prefabs.
+*due to christmas timing I have not updated the example scene. I will try and get it done as soon as possible for people to look through.
+
+V1.2.0
+*fly stick should reset your gravity if you turn gravity dissable off during flight.
+*added set portal id prefab.
+*added strafe speed to vrcworld settings prefab.
+*vrcworld voice settings added + zones.
+*world user list automatically sets size of needed array.
+*changed round portal world id.
+
+
+V1.1.1
+*updated prefabs with player triggers to new proper events
+*updated prefabs that had ui events not working.
+*upgraded projector decal shader
+*clocks now have am/pm included in display
+
+V1.1.0
+*added groups and comments to some graphs
+*join sound prefab
+*fly stick update (now can disable gravity while flying)
+*datetime ui text updated
+*instance master identifier prefab
+*fixed key lock prefab
+*layer switch prefab/example
+*local axis movement prefab/example
+*navagent follow prefab
+*player teleport button prefab
+*random int prefab/example
+*set bool ui prefab
+*set material prefab/example
+*specific user join prefab/example
+*station button prefab
+*vrcworld settings edit prefab
+*player mods edit variant prefab
+*postprocessing zone prefab/example
+*grip pickups examples
+*station rotation fix
+
+V1.0.0
+new prefabs:
+*2d background audio
+*bounce pad
+*button event
+*constraints example
+*Datetime display clock
+*decal light
+*camera reference
+*fly stick
+*fly stick setup
+*headtracking proxy
+*info panel
+*keylock
+*keypad
+*lighting preset
+*mirror toggle
+*pickup example
+*reset pickup
+*round portal
+*simple toggle
+*synced pickup example
+*synced switch
+*teleport pad
+*teleport stick
+*twoway teleport pad
+*ui float set
+*ui panel blank
+*ui panel example
+*unity package
+*vrcplayer collider proxy
---
+++ Hellohl - Quest/Assets/VRWorldToolkit/Scripts/Editor/VRWTDataStructures.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+
+namespace VRWorldToolkit.DataStructures
+{
+    public static class Styles
+    {
+        public static GUIStyle HelpBoxRichText { get; internal set; }
+        public static GUIStyle HelpBoxPadded { get; internal set; }
+        public static GUIStyle LabelRichText { get; internal set; }
+        public static GUIStyle RichText { get; internal set; }
+        public static GUIStyle RichTextWrap { get; internal set; }
+        public static GUIStyle BoldWrap { get; internal set; }
+        public static GUIStyle RedLabel { get; internal set; }
+        public static GUIStyle WhiteLabel { get; internal set; }
+        public static GUIStyle TreeViewLabel { get; internal set; }
+        public static GUIStyle TreeViewLabelSelected { get; internal set; }
+        public static GUIStyle CenteredLabel { get; internal set; }
+
+        static Styles()
+        {
+            Reload();
+        }
+
+        static void Reload()
+        {
+            HelpBoxRichText = new GUIStyle("HelpBox")
+            {
+                richText = true
+            };
+
+            HelpBoxPadded = new GUIStyle("HelpBox")
+            {
+                margin = new RectOffset(18, 4, 4, 4),
+                alignment = TextAnchor.MiddleLeft,
+                richText = true
+            };
+
+            LabelRichText = new GUIStyle("Label")
+            {
+                richText = true,
+                margin = new RectOffset(5, 5, 0, 0),
+            };
+
+            RichText = new GUIStyle
+            {
+                richText = true
+            };
+
+            RichTextWrap = new GUIStyle
+            {
+                richText = true,
+                wordWrap = true
+            };
+
+            BoldWrap = new GUIStyle("boldLabel")
+            {
+                wordWrap = true
+            };
+
+            RedLabel = new GUIStyle
+            {
+                normal =
+                {
+                    textColor = Color.red,
+                },
+            };
+
+            TreeViewLabel = new GUIStyle("Label")
+            {
+                alignment = TextAnchor.MiddleLeft,
+                wordWrap = false,
+            };
+
+            TreeViewLabelSelected = new GUIStyle("WhiteLabel")
+            {
+                alignment = TextAnchor.MiddleLeft,
+                wordWrap = false,
+            };
+
+            CenteredLabel = new GUIStyle("Label")
+            {
+                alignment = TextAnchor.LowerCenter,
+                fontSize = 17,
+                fontStyle = FontStyle.BoldAndItalic,
+                normal =
+                {
+                    textColor = new Color(0.33f, 0.33f, 0.33f),
+                }
+            };
+        }
+    }
+
+    public static class Validation
+    {
+        public static readonly string[] WorldShaderWhiteList = new string[]
+        {
+            "VRChat/Mobile/Standard Lite",
+            "VRChat/Mobile/Diffuse",
+            "VRChat/Mobile/Bumped Diffuse",
+            "VRChat/Mobile/Bumped Mapped Specular",
+            "VRChat/Mobile/Toon Lit",
+            "VRChat/Mobile/MatCap Lit",
+            "VRChat/Mobile/Lightmapped",
+            "VRChat/Mobile/Skybox",
+            "VRChat/Mobile/Particles/Additive",
+            "VRChat/Mobile/Particles/Multiply",
+            "FX/MirrorReflection",
+            "UI/Default",
+        };
+    }
+}
